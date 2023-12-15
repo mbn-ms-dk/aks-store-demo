202b5d468308bf432a85b9c0aee06dfec0c29090
Mikkel Bach Nielsen <103189707+mbn-ms-dk@users.noreply.github.com>
2023-12-15 15:24:49 +0100

    Base (#1)
    * base
    
    * Update dependencies and configuration

--- .devcontainer/installMoreTools.sh
+++ .devcontainer/installMoreTools.sh
@@ -11,3 +11,6 @@ sudo apt-get update && sudo apt-get install uuid-runtime
 wget -qO- https://www.mongodb.org/static/pgp/server-7.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-7.0.asc
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update && sudo apt-get install -y mongodb-mongosh
+echo "installing kubeshark"
+sudo apt-get install curl
+curl -Ls https://kubeshark.co/install
